5d51a421358523055462eab7bcafbcec8f28e452
Brandon Castro <66087694+liljGremlin@users.noreply.github.com>
2023-07-20 18:36:51 -0700

    Update mailModule.js

--- myModules/mailModule.js
+++ myModules/mailModule.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-const rootURL = process.dev.ROOTURL
+const rootURL = process.env.ROOTURL
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
